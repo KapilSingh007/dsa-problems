7423680994419c6dd0256128d596eacbff00cae0
KapilSingh007 <kapilsingh3467@gmail.com>
2025-10-20 23:57:52 +0530

    Count Partitions with Given Difference

---
+++ dp/Count-Partitions-with-Given-Difference.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    public:
+      int countPartitions(vector<int>& arr, int d) {
+          // Code here
+          
+          int sum=0,n=arr.size();
+          for(int i=0;i<n;i++){
+              sum+= arr[i];
+          }
+          
+          if((sum-d) %2 !=0  || sum-d<0)return 0;
+          
+          int target = (sum-d)/2;
+          
+          vector<vector<int>> dp(n+1,vector<int>(target+1,0));
+          
+          
+          dp[0][0] = 1;
+          
+          for(int i=1;i<=n;i++){
+              for(int j=0;j<=target;j++){
+                  dp[i][j] = dp[i-1][j];
+                  
+                  if(arr[i-1] <= j){
+                      dp[i][j] += dp[i-1][j-arr[i-1]];
+                  }
+              }
+          }
+          
+          return dp[n][target];
+          
+      }
+  };
