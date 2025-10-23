5d1da71df80d3c1915c3876257626fe5decf21cf
KapilSingh007 <kapilsingh3467@gmail.com>
2025-10-23 23:27:28 +0530

    coins change 1 D dp

--- dp/coins-change.cpp
+++ dp/coins-change.cpp
@@ -12,9 +12,7 @@ class Solution {
                         dp[i][j] = dp[i - 1][j];
                     } else {
                         if (dp[i - 1][j] == -1) {
-                            if (dp[i][j - coins[i - 1]] == -1) {
-                                dp[i][j] = -1;
-                            } else {
+                            if (dp[i][j - coins[i - 1]] != -1) {
                                 dp[i][j] = 1 + dp[i][j - coins[i - 1]];
                             }
                         } else {
@@ -30,13 +28,35 @@ class Solution {
             }
             return dp[n][amount];
         }
+    
+        int oneDDP(vector<int>& coins, int amount, int n) {
+            vector<int> dp(amount + 1, -1);
+            dp[0] = 0;
+    
+            for (int i = 1; i <= n; i++) {
+                for (int j = coins[i - 1]; j <= amount; j++) {
+                    if (dp[j] == -1) {
+                        if (dp[j - coins[i - 1]] != -1) {
+                            dp[j] = 1 + dp[j - coins[i - 1]];
+                        }
+                    } else {
+                        if (dp[j - coins[i - 1]] != -1) {
+                            dp[j] = min(dp[j], 1 + dp[j - coins[i - 1]]);
+                        }
+                    }
+                }
+            }
+    
+            return dp[amount];
+        }
         int coinChange(vector<int>& coins, int amount) {
             if (amount == 0)
                 return 0;
     
             int n = coins.size();
     
-            return twoDDp(coins,amount,n);
+            // return twoDDp(coins,amount,n);
+            return oneDDP(coins,amount,n);
     
             
         }
