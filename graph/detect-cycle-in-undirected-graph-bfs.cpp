2d1daf312652a0099be7d37c3efadb88ab3146d1
KapilSingh007 <kapilsingh3467@gmail.com>
2025-10-29 21:22:45 +0530

    detect-cycle-in-undirected-graph-bfs

---
+++ graph/detect-cycle-in-undirected-graph-bfs.cpp
@@ -0,0 +1,64 @@
+class Solution {
+    public:
+    
+      vector<vector<int>> creationOfAdjList(int V,vector<vector<int>>& edges){
+          vector<vector<int>> adj(V);
+          
+          int n=edges.size();
+          for(int i=0;i<n;i++){
+              adj[edges[i][0]].push_back(edges[i][1]);
+              adj[edges[i][1]].push_back(edges[i][0]);
+          }
+          return adj;
+      }
+      bool bfs(int V, vector<vector<int>>& edges){
+          vector<vector<int>> adj = creationOfAdjList(V,edges);
+          vector<int> vis(V,0);
+          queue<pair<int,int>> q;
+          int qCurrSz , node, parent, childArrsize;
+          
+          for(int i=0;i<V;i++){
+              if(vis[i]){
+                  continue;
+              }
+              q.push({i,-1});
+              vis[i] = 1;
+              
+              while(!q.empty()){
+                  qCurrSz = q.size();
+                  
+                  while(qCurrSz--){
+                      node = q.front().first;
+                      parent = q.front().second;
+                      q.pop();
+                      
+                      childArrsize = adj[node].size();
+                      for(int j=0;j<childArrsize;j++){
+                          if(adj[node][j] == parent){
+                              continue;
+                          }
+                          if(vis[adj[node][j]]){
+                              return true;
+                          }
+                          
+                          q.push({adj[node][j],node});
+                          vis[adj[node][j]] = 1;
+                      }
+                  }
+              }
+          }
+          return false;
+      }
+      
+      
+      bool dfs(int V, vector<vector<int>>& edges){
+          return false;
+      }
+      bool isCycle(int V, vector<vector<int>>& edges) {
+          // Code here
+          
+          return bfs(V,edges);
+          // return dfs(V,edges);
+          
+      }
+  };
