2d995b3b9988d9f24e1ed9e10c02a5f2e1807417
KapilSingh007 <kapilsingh3467@gmail.com>
2025-11-09 17:42:04 +0530

    tabulation code

--- dp/123-Best-time-to-Buy-and-Sell-Stock-III.cpp
+++ dp/123-Best-time-to-Buy-and-Sell-Stock-III.cpp
@@ -1,7 +1,7 @@
 class Solution {
     public:
         int approach1Recurssion(vector<int>& p, int& n, int i, int txn, int isBuyed,
-                                int lastEle, vector<vector<vector<int>>>& dp) {
+                                vector<vector<vector<int>>>& dp) {
     
             if (txn >= 2 || i == n) {
                 return 0;
@@ -12,22 +12,44 @@ class Solution {
             }
             if (isBuyed) {
                 return dp[i][isBuyed][txn] = max(
-                           p[i] +
-                               approach1Recurssion(p, n, i + 1, txn + 1, 0, -1, dp),
-                           approach1Recurssion(p, n, i + 1, txn, 1, lastEle, dp));
+                           p[i] + approach1Recurssion(p, n, i + 1, txn + 1, 0, dp),
+                           approach1Recurssion(p, n, i + 1, txn, 1, dp));
             } else {
-                return dp[i][isBuyed][txn] = max(
-                           -p[i] +
-                               approach1Recurssion(p, n, i + 1, txn, 1, p[i], dp),
-                           approach1Recurssion(p, n, i + 1, txn, 0, lastEle, dp));
+                return dp[i][isBuyed][txn] =
+                           max(-p[i] + approach1Recurssion(p, n, i + 1, txn, 1, dp),
+                               approach1Recurssion(p, n, i + 1, txn, 0, dp));
             }
         }
     
-        
+        int approach2Tabulation(vector<int>& p, int n) {
+            vector<vector<vector<int>>> dp(
+                n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+    
+            for (int i = n-1; i >=0 ; i--) {
+                for (int isBuyed = 0; isBuyed < 2; isBuyed++) {
+                    for (int txn = 1; txn <= 2; txn++) {
+                        if (isBuyed) {
+                            dp[i][isBuyed][txn] =
+                                max(p[i] + dp[i + 1][!isBuyed][txn - 1],
+                                    dp[i + 1][isBuyed][txn]);
+                        } else {
+    
+                            dp[i][isBuyed][txn] =
+                                max(-p[i] + dp[i + 1][!isBuyed][txn],
+                                    dp[i + 1][isBuyed][txn]);
+                        }
+                    }
+                }
+            }
+    
+            return dp[0][0][2];
+        }
         int maxProfit(vector<int>& p) {
             int n = p.size();
-            vector<vector<vector<int>>> dp(
-                n, vector<vector<int>>(2, vector<int>(3, -1)));
-            return approach1Recurssion(p, n, 0, 0, 0, -1, dp);
+            // vector<vector<vector<int>>> dp(
+            //     n, vector<vector<int>>(2, vector<int>(3, -1)));
+            // return approach1Recurssion(p, n, 0, 0, 0, dp);
+    
+            return approach2Tabulation(p, n);
         }
     };
--- dp/188-Best-Time-to-Buy-and-Sell-Stock-IV.cpp
+++ dp/188-Best-Time-to-Buy-and-Sell-Stock-IV.cpp
@@ -21,10 +21,37 @@ class Solution {
                            approach1Recurssion(p, n, k, i + 1, txn, 0, dp));
             }
         }
+    
+        int approach2Tabulation(vector<int>& p, int n, int k) {
+            vector<vector<vector<int>>> dp(
+                n + 1, vector<vector<int>>(2, vector<int>(k + 1, 0)));
+    
+            for (int i = n - 1; i >= 0; i--) {
+                for (int isBuyed = 0; isBuyed < 2; isBuyed++) {
+                    for (int txn = 1; txn <= k; txn++) {
+                        if (isBuyed) {
+                            dp[i][isBuyed][txn] =
+                                max(p[i] + dp[i + 1][!isBuyed][txn - 1],
+                                    dp[i + 1][isBuyed][txn]);
+                        } else {
+    
+                            dp[i][isBuyed][txn] =
+                                max(-p[i] + dp[i + 1][!isBuyed][txn],
+                                    dp[i + 1][isBuyed][txn]);
+                        }
+                    }
+                }
+            }
+    
+            return dp[0][0][k];
+        }
+    
         int maxProfit(int k, vector<int>& p) {
             int n = p.size();
-            vector<vector<vector<int>>> dp(
-                n, vector<vector<int>>(2, vector<int>(k, -1)));
-            return approach1Recurssion(p, n, k, 0, 0, 0, dp);
+            // vector<vector<vector<int>>> dp(
+            //     n, vector<vector<int>>(2, vector<int>(k, -1)));
+            // return approach1Recurssion(p, n, k, 0, 0, 0, dp);
+    
+            return approach2Tabulation(p, n, k);
         }
     };
