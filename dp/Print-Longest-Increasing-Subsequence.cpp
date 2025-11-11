2b271075590058592bbf6b570bc935bc641e0f8e
KapilSingh007 <kapilsingh3467@gmail.com>
2025-11-12 00:28:43 +0530

    print lis approach 2

--- dp/Print-Longest-Increasing-Subsequence.cpp
+++ dp/Print-Longest-Increasing-Subsequence.cpp
@@ -27,12 +27,45 @@ class Solution {
     
             return res;
         }
+
+        vector<int> approach2Tabulation(vector<int>& arr, int n) {
+
+            vector<int> dp(n, 1),prevInd(n,-1);
+         
+            int mx = 1,mxInd=0;
+            vector<int> res;
+    
+            for (int i = 1; i < n; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (arr[i] > arr[j] && dp[i] < dp[j] + 1) {
+                        dp[i] = dp[j] + 1;
+                        prevInd[i] = j;
+                    }
+                }
+    
+                if(mx<dp[i]){
+                    mx = dp[i];
+                    mxInd = i;
+                }
+            }
+            
+            while(mxInd != -1){
+                res.insert(res.begin(),arr[mxInd]);
+                mxInd = prevInd[mxInd];
+            }
+           
+    
+            return res;
+        }
         vector<int> getLIS(vector<int>& arr) {
             // Code here
             int n = arr.size();
     
            // below approach is corrent but it will give TLE in the last test cases and work for the most of the test cases
-            return approach1Tabulation(arr, n);
+            // return approach1Tabulation(arr, n);
+
+
+            return approach2Tabulation(arr, n);
 
         }
     };
