39768ccf540e48c2a391334f146b7414fbade33b
KapilSingh007 <kapilsingh3467@gmail.com>
2025-11-28 00:01:22 +0530

    topological sort using bfs

--- graph/topological-sort.cpp
+++ graph/topological-sort.cpp
@@ -1,6 +1,6 @@
 class Solution {
     public:
-        vector<vector<int>> createAdj(vector<vector<int>>& edges,int V) {
+        vector<vector<int>> createAdj(vector<vector<int>>& edges, int V) {
             int n = edges.size();
     
             vector<vector<int>> adj(V, vector<int>());
@@ -8,7 +8,7 @@ class Solution {
             for (int i = 0; i < n; i++) {
                 adj[edges[i][0]].push_back(edges[i][1]);
             }
-            
+    
             return adj;
         }
     
@@ -25,18 +25,57 @@ class Solution {
     
             res.insert(res.begin(), currNode);
         }
-        vector<int> topoSort(int V, vector<vector<int>>& edges) {
-            // code here
     
-            vector<int> res, vis(V, 0);
+        void approach1DFS(int V, vector<vector<int>>& adj, vector<int>& res) {
     
-            vector<vector<int>> adj = createAdj(edges,V);
-    
+            vector<int> vis(V, 0);
             for (int i = 0; i < V; i++) {
                 if (!vis[i]) {
                     dfs(adj, i, res, vis);
                 }
             }
+        }
+    
+        void approach1BFS(int V, vector<vector<int>>& edges,
+                          vector<vector<int>>& adj, vector<int>& res) {
+    
+            vector<int> indegree(V, 0);
+            int e = edges.size(), node;
+            queue<int> q;
+            for (int i = 0; i < e; i++) {
+                indegree[edges[i][1]]++;
+            }
+    
+            for (int i = 0; i < V; i++) {
+                if (indegree[i] == 0) {
+                    q.push(i);
+                }
+            }
+    
+            while (!q.empty()) {
+                node = q.front();
+                q.pop();
+    
+                res.push_back(node);
+    
+                for (int x : adj[node]) {
+    
+                    if (--indegree[x] == 0) {
+                        q.push(x);
+                    }
+                }
+            }
+        }
+        vector<int> topoSort(int V, vector<vector<int>>& edges) {
+            // code here
+    
+            vector<int> res;
+    
+            vector<vector<int>> adj = createAdj(edges, V);
+    
+            // approach1DFS(V, adj, res);
+    
+            approach1BFS(V, edges, adj, res);
     
             return res;
         }
