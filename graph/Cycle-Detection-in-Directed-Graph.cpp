70ef037d7c57b3b2fdbff8953bb4470e2e12cf9a
KapilSingh007 <kapilsingh3467@gmail.com>
2025-11-29 20:47:34 +0530

    cyclic in directed graph using bfs

--- graph/Cycle-Detection-in-Directed-Graph.cpp
+++ graph/Cycle-Detection-in-Directed-Graph.cpp
@@ -12,15 +12,15 @@ class Solution {
             return adj;
         }
     
-        bool dfs(vector<vector<int>>& adj, int V, vector<int>& vis,
-                 vector<int>& inRecurr, int currNode) {
+        bool dfsHelper(vector<vector<int>>& adj, int V, vector<int>& vis,
+                       vector<int>& inRecurr, int currNode) {
     
             vis[currNode] = 1;
             inRecurr[currNode] = 1;
     
             for (int x : adj[currNode]) {
                 if (!vis[x]) {
-                    if (dfs(adj, V, vis, inRecurr, x)) {
+                    if (dfsHelper(adj, V, vis, inRecurr, x)) {
                         return true;
                     }
                 } else if (inRecurr[x] == 1) {
@@ -31,17 +31,57 @@ class Solution {
             inRecurr[currNode] = 0;
             return false;
         }
-        bool isCyclic(int V, vector<vector<int>>& edges) {
-            // code here
+    
+        bool dfs(vector<vector<int>> adj, int V) {
             vector<int> vis(V, 0), inRecurr(V, 0);
-            vector<vector<int>> adj = createAdj(edges, V);
             for (int i = 0; i < V; i++) {
     
-                if (!vis[i] && dfs(adj, V, vis, inRecurr, i)) {
+                if (!vis[i] && dfsHelper(adj, V, vis, inRecurr, i)) {
                     return true;
                 }
             }
     
             return false;
+        }
+    
+        bool bfs(vector<vector<int>>& edges, vector<vector<int>> adj, int V) {
+    
+            int n = edges.size(), currNode;
+            vector<int> indegree(V, 0), res;
+            queue<int> q;
+    
+            for (int i = 0; i < n; i++) {
+                indegree[edges[i][1]]++;
+            }
+    
+            for (int i = 0; i < V; i++) {
+                if (indegree[i] == 0) {
+                    q.push(i);
+                    res.push_back(i);
+                }
+            }
+    
+            while (!q.empty()) {
+                currNode = q.front();
+                q.pop();
+    
+                for (int x : adj[currNode]) {
+                    if (--indegree[x] == 0) {
+                        q.push(x);
+                        res.push_back(x);
+                    }
+                }
+            }
+    
+            return res.size() != V;
+        }
+    
+        bool isCyclic(int V, vector<vector<int>>& edges) {
+            // code here
+    
+            vector<vector<int>> adj = createAdj(edges, V);
+            // return dfs(adj, V);
+    
+            return bfs(edges, adj, V);
         }
     };
