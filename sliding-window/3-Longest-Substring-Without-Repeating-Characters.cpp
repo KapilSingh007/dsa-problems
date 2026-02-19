77d48cd0369144a7589bc5964716110113b970b0
KapilSingh007 <kapilsingh3467@gmail.com>
2026-02-19 21:49:00 +0530

    practised 3. Longest Substring Without Repeating Characters

--- sliding-window/3-Longest-Substring-Without-Repeating-Characters.cpp
+++ sliding-window/3-Longest-Substring-Without-Repeating-Characters.cpp
@@ -1,23 +1,22 @@
 class Solution {
     public:
         int lengthOfLongestSubstring(string s) {
-            int i = 0, j = 0, res = 0, n = s.size();
+    
             unordered_map<char, int> mp;
+    
+            int i = 0, j = 0, n = s.size(), res = 0;
+    
             while (i < n) {
                 mp[s[i]]++;
-                if (res < mp.size()) {
-                    res = mp.size();
-                }
+    
                 while (mp[s[i]] > 1) {
                     mp[s[j]]--;
-                    if (mp[s[j]] == 0) {
-                        mp.erase(s[j]);
-                    }
                     j++;
                 }
-                i++;
-            }
     
+                i++;
+                res = max(res, i - j);
+            }
             return res;
         }
     };
